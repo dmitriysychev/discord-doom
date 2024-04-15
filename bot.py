ea96d2665d6660d8d47adbaa4cced8f27f015267
Dmitriy Sychev <dmitriy.sychev@austin.utexas.edu>
2024-04-14 22:27:18 -0600

    Update to on member join event

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import discord
 import asyncio
 import datetime
 import requests
-
+import openai
 import os
 # import socket
 
@@ -13,6 +13,12 @@ from opengsq.protocols import Source
 from discord.ext import commands
 import aiohttp
 import asyncio
+
+import logging
+
+logging.basicConfig(level=logging.INFO, filename='bot_log.log', filemode='a', format='%(name)s - %(levelname)s - %(message)s')
+
+
 # Imports END
 gpt_headers =  {
     'content-type': 'application/json',
@@ -22,67 +28,97 @@ gpt_headers =  {
   }
 webhook_url = "https://discord.com/api/webhooks/1220067617868484729/1yxYhBAwvKqb5aJtlRbVh-usbwwRqoF93vzc4GmL19VWTbc3-cJwOpsaQ0Z2JbI1DIfS"
 
-
+conversations = {}
 # Init variables
 load_dotenv()
+openai.api_key = os.getenv('OPENAI_API_KEY')
 TOKEN = os.getenv('TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 GUILD_ID = os.getenv('GUILD_ID')
+JOIN_CHANNEL_ID = os.getenv('JOIN_CHANNEL_ID')
 
 # Client start
 bot = commands.Bot(command_prefix='!',intents=discord.Intents.all())
 
-async def load_extension():
+async def load_extensions():
     for f in os.listdir("./cogs"):
         if f.endswith(".py"):
-            await bot.load_extension("cogs." + f[:-3])
-                        
+            bot.load_extension(f"cogs.{f[:-3]}")
+                    
     
 
 # Bot EVENTS
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to the {GUILD}')
-    
-@commands.command(name="ау")
-async def talk(ctx, *args):
-    body = {
-        "model": "gpt-3.5-turbo",
-        "messages": [
-            {
-                "role": "user",
-                "content": " ".join(args)
-            }
-        ]
-    }
 
-    response = requests.post("https://chat-gpt26.p.rapidapi.com/", headers=gpt_headers, json=body)
-    data = response.json()['choices'][0]['message']['content']
+@bot.event
+async def on_member_join(member):
+    logging.info(f"Member joined: {member}")
+    channel = bot.get_channel(int(JOIN_CHANNEL_ID))
+    logging.info(f"Channel: {channel}")
 
-    await ctx.send(data)
+    print("Channel: ", channel)
+    if channel:
+        embed = discord.Embed(title="Здарова Сталкер!", description=f"{member.mention} вошел в Зону Отчуждения", color=0x00ff00)
+        embed.add_field(name="Аккаунт создан", value=member.created_at.strftime("%A, %B %d, %Y at %H:%M %p"), inline=True)
+        if getattr(member, 'avatar_url', None) is not None:
+            embed.set_thumbnail(url=member.avatar_url)
+        await channel.send(member.mention)
+        await channel.send(embed=embed)
+
+@bot.event
+async def on_message(message):
+    # Не отвечаем на сообщения от самого бота
+    if message.content.startswith('!ау'):
+        channel_id = message.channel.id
+            
+            # Retrieve the current conversation history for the channel
+        if channel_id in conversations:
+            chat_history = conversations[channel_id]
+        else:
+            chat_history = []
+            conversations[channel_id] = chat_history
+        
+        user_query = message.content[len('!ау '):]
+        
+        # Persistent character context and introduction for new conversation
+        if not chat_history:  # if conversation history is empty
+            context = "The year is 2008. I am Doom, a stalker in the Chernobyl Exclusion Zone. Your answer needs to be no more than 317 characters."
+            chat_history.append({"role": "system", "content": context})
+
+        chat_history.append({"role": "user", "content": user_query})
+
+            # Construct the prompt with all preceding messages
+        async with message.channel.typing():
+            response = openai.ChatCompletion.create(
+                model="gpt-4-turbo",
+                messages=chat_history,
+                max_tokens=150
+            )
+
+            # Get the response and add it to history
+            bot_response = response['choices'][0]['message']['content']
+            chat_history.append({"role": "assistant", "content": bot_response})
+
+    # Send the response to the Discord channel
+            await asyncio.sleep(3)
+            await message.channel.send(bot_response)
+
 
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.errors.CheckFailure):
         await ctx.send('У вас нет необходимой роли для выполнения этой команды')
 
-@bot.event
-async def on_member_join(member):
-    # await bot.send_message(member, 'Prompt.')
-    # m = await bot.wait_for_message(author=member, channel=member)
-    # if m.content == 'key':
-    #     # give the user the role
-    #     await bot.send_message(member, 'Role added')
-    # else:
-    #     await bot.send_message(member, 'Incorrect key')
-    pass
-bot.add_command(talk)
 
 async def main():
     async with bot:
-        await load_extension()
+        await load_extensions()
         await bot.start(TOKEN)
-# bot.run(TOKEN)
+
+asyncio.run(main())
+
 
 
      
