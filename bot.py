8eed5dd5d94e155a59b03ac4dde3cea9d53346dc
Dmitriy Sychev <dmitriy.sychev@austin.utexas.edu>
2024-04-14 23:02:54 -0600

    Achievement system

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ import datetime
 import requests
 import openai
 import os
+import sqlite3
 # import socket
 
 from dotenv import load_dotenv
@@ -29,6 +30,12 @@ gpt_headers =  {
 webhook_url = "https://discord.com/api/webhooks/1220067617868484729/1yxYhBAwvKqb5aJtlRbVh-usbwwRqoF93vzc4GmL19VWTbc3-cJwOpsaQ0Z2JbI1DIfS"
 
 conversations = {}
+achievements = {
+    "Опытный мужик": {"message_count": 500, "description": "Отправил 500 сообщений!"},
+    "Сталкер со стажем": {"voice_minutes": 200, "description": "Потратил 200 минут в войс чате!"}
+}
+
+
 # Init variables
 load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
@@ -39,6 +46,12 @@ JOIN_CHANNEL_ID = os.getenv('JOIN_CHANNEL_ID')
 
 # Client start
 bot = commands.Bot(command_prefix='!',intents=discord.Intents.all())
+conn = sqlite3.connect('achievements.db')
+c = conn.cursor()
+
+# Create table
+c.execute('''CREATE TABLE IF NOT EXISTS user_activity (user_id INTEGER PRIMARY KEY, message_count INTEGER, voice_minutes INTEGER, achievements TEXT)''')
+conn.commit()
 
 async def load_extensions():
     for f in os.listdir("./cogs"):
@@ -46,6 +59,21 @@ async def load_extensions():
             bot.load_extension(f"cogs.{f[:-3]}")
                     
     
+async def check_achievements(user_id):
+    c.execute('SELECT message_count, voice_minutes, achievements FROM user_activity WHERE user_id = ?', (user_id,))
+    data = c.fetchone()
+    current_achievements = data[2].split(',') if data[2] else []
+    
+    for key, value in achievements.items():
+        if key not in current_achievements:
+            if (value.get('message_count', float('inf')) <= data[0] or
+                value.get('voice_minutes', float('inf')) <= data[1]):
+                current_achievements.append(key)
+                await bot.get_user(user_id).send(f"Поздравляю! Ты получил достижение '{key}' за то что {value['description']}")
+
+    # Update achievements in the database
+    c.execute('UPDATE user_activity SET achievements = ? WHERE user_id = ?', (','.join(current_achievements), user_id))
+    conn.commit()
 
 # Bot EVENTS
 @bot.event
@@ -56,9 +84,6 @@ async def on_ready():
 async def on_member_join(member):
     logging.info(f"Member joined: {member}")
     channel = bot.get_channel(int(JOIN_CHANNEL_ID))
-    logging.info(f"Channel: {channel}")
-
-    print("Channel: ", channel)
     if channel:
         embed = discord.Embed(title="Здарова Сталкер!", description=f"{member.mention} вошел в Зону Отчуждения", color=0x00ff00)
         embed.add_field(name="Аккаунт создан", value=member.created_at.strftime("%A, %B %d, %Y at %H:%M %p"), inline=True)
@@ -69,7 +94,21 @@ async def on_member_join(member):
 
 @bot.event
 async def on_message(message):
-    # Не отвечаем на сообщения от самого бота
+    if message.author == bot.user:
+        return
+    
+    # Update message count in the database
+    c.execute('SELECT message_count FROM user_activity WHERE user_id = ?', (message.author.id,))
+    data = c.fetchone()
+    if data:
+        logging.info(f"Data found for user{message.author} -  {data}")
+        c.execute('UPDATE user_activity SET message_count = message_count + 1 WHERE user_id = ?', (message.author.id,))
+    else:
+        logging.info(f"Data not found found for user{message.author} - incrementing message count")
+        c.execute('INSERT INTO user_activity (user_id, message_count, voice_minutes, achievements) VALUES (?, 1, 0, "")', (message.author.id,))
+    conn.commit()
+    await check_achievements(message.author.id)
+    #Не отвечаем на сообщения от самого бота
     if message.content.startswith('!ау'):
         channel_id = message.channel.id
             
